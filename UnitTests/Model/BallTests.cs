7b25fdbe99425c0e7dc83b9125e4b67842945c22
newProf1 <elfearvengefulro12@gmail.com>
2022-06-27 06:50:26 +0300

    Создание тестов для класса Ball

---
+++ UnitTests/Model/BallTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Model;
+namespace UnitTests.Model
+{
+    [TestFixture]
+    class BallTests
+    {
+        [Test]
+        [TestCase(-1, TestName = "Side1 = -1")]
+        [TestCase(0, TestName = "Side1 = 0")]
+        [TestCase(1, TestName = "Side1 = 1")]
+        [TestCase(1000, TestName = "Side1 = 1000")]
+        [TestCase(10001, TestName = "Side1 = 10001")]
+        public void BTest1(int r)
+        {
+            var test = new Ball(r);
+        }
+    }
+}
