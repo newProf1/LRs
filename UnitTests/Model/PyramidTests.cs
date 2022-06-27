2761b47f0530207688bf2a427ee42bc2a99f9009
newProf1 <elfearvengefulro12@gmail.com>
2022-06-27 06:50:52 +0300

    Создание тестов для класса Pyramid

---
+++ UnitTests/Model/PyramidTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Model;
+namespace UnitTests.Model
+{
+    [TestFixture]
+    class PyramidTests
+    {
+        [Test]
+        [TestCase(-1, TestName = "Side2 = -1")]
+        [TestCase(0, TestName = "Side2 = 0")]
+        [TestCase(1, TestName = "Side2 = 1")]
+        [TestCase(1000, TestName = "Side2 = 1000")]
+        [TestCase(10001, TestName = "Side2 = 10001")]
+        public void PTestS2(int h)
+        {
+            var test = new Pyramid(1, h);
+        }
+    }
+}
