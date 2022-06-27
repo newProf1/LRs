f378d0ddcc836f0785e916e51132920fa26d998a
newProf1 <elfearvengefulro12@gmail.com>
2022-06-27 06:51:09 +0300

    Создание тестов для класса Parallelepiped

---
+++ UnitTests/Model/ParallelepipedTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Model;
+namespace UnitTests.Model
+{
+    [TestFixture]
+    class ParallelepipedTests
+    {
+        [Test]
+        [TestCase(-1, TestName = "Side3 = -1")]
+        [TestCase(0, TestName = "Side3 = 0")]
+        [TestCase(1, TestName = "Side3 = 1")]
+        [TestCase(1000, TestName = "Side3 = 1000")]
+        [TestCase(10001, TestName = "Side3 = 10001")]
+        public void PTestS3(int s3)
+        {
+            var test = new Parallelepiped(1, 1, s3);
+        }
+    }
+}
